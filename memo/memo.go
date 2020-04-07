9c93ba709dbe284114a7c6ba321e73a0d28a0da0
Averkij <averoo@gmail.com>
2020-04-07 09:57:19 +0300

    Update memory trainer.

--- memo/memo.go
+++ memo/memo.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
-	fmt.Println("memo")
+	seed := rand.NewSource(time.Now().UnixNano())
+	rnd := rand.New(seed)
+	intervalInSeconds := 2
+
+	for i := 0; i < 10; i++ {
+		fmt.Print(rnd.Int31n(100))
+		time.Sleep(time.Second * time.Duration(intervalInSeconds))
+		fmt.Print(" - ")
+		time.Sleep(time.Second)
+	}
 }
